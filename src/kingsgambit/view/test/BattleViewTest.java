ace99cafff6d22976dad148b83608d46250e627f
John Charles Bauschatz <john.bauschatz@my.wheaton.edu>
2013-12-07 23:23:59 -0600

    Corrected BattleViewTest error, which had not been tested after a recent refactor.

--- src/kingsgambit/view/test/BattleViewTest.java
+++ src/kingsgambit/view/test/BattleViewTest.java
@@ -1,22 +1,18 @@
 package kingsgambit.view.test;
 
 import kingsgambit.controller.BattleController;
-import kingsgambit.model.Battle;
 import kingsgambit.model.BattleParameters;
 import kingsgambit.model.PlayerControlOption;
 import kingsgambit.view.battle.BattleView;
 
 public class BattleViewTest {
 	public static void main(String... args) {
-		Battle battle = new Battle(new BattleParameters(PlayerControlOption.HUMAN, PlayerControlOption.EASY));
-
 		BattleParameters parameters = new BattleParameters(PlayerControlOption.HUMAN, PlayerControlOption.EASY);
-		
 		BattleController controller = new BattleController(parameters);
 
 		BattleView battleView = new BattleView(controller);
 		controller.setView(battleView);
 		
-		battle.begin();
+		controller.getBattle().begin();
 	}
 }
